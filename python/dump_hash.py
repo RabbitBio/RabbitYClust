f92174c4a39dd1dd6afb69ee502ae15a968546a5
yzk <zekun.yin@mail.sdu.edu.cn>
2024-08-01 00:45:46 +0800

    single threaded imp for generate hashes using KHF

---
+++ python/dump_hash.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import struct
+import sys
+
+def read_uint64_from_file(filename):
+    try:
+        with open(filename, 'rb') as f:
+            while chunk := f.read(8):  # Read 8 bytes at a time (64 bits)
+                if len(chunk) < 8:
+                    print(f"Incomplete data: {chunk}")
+                    continue
+                value = struct.unpack('<Q', chunk)[0]  # Little-endian unsigned 64-bit integer
+                print(value)
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 dump_hash.py <binary_file>")
+        sys.exit(1)
+
+    binary_file = sys.argv[1]
+    read_uint64_from_file(binary_file)
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,99 @@
 #include "KHFMinHash.h"
+#include "kseq.h"
+#include <zlib.h>
 
 #include <iostream>
 #include <string>
+#include <cstring>
+#include <fstream>
 
 using namespace std;
 using namespace Sketch;
 
-int main()
+KSEQ_INIT(gzFile, gzread)
+
+int main(int argc, char** argv)
 {
-	char test[] = "LKPYLDKGDIIIDGGNTFFQDTIRRNRELSAEGFNFIGTGVSGGEEGALKGPSIMPGGQKEAYELVAPILTKIAAVAEDGLKPYLDKGDIIIDGGNTFFQDTIRRNRELSAEGFNFIGTGVSGGEEGALKGPSIMPGGQKEAYELVAPILTKIAAVAEDG";
 
-	KHFMinHash mh = KHFMinHash(test);			
+	if(argc != 3)
+	{
+		cerr << "Usage: ./yclust input.fa output.hash" << endl;
+		cerr << "Note: output.hash is in binary format for uint64_t hash values!" << endl;
+		cerr << "Note: the names of sequences are printed to stdout!" << endl;
+		return 1;
+	}
+	gzFile fp1;
+	kseq_t *ks1;
+
+	fp1 = gzopen(argv[1],"r");
+
+	if(NULL == fp1){
+		fprintf(stderr,"Fail to open file: %s\n", argv[1]);
+		return 0;
+	}
+
+
+	ofstream ofile(string(argv[2]), ios::binary);
+
+	if( !ofile.is_open())
+	{
+		cerr << "Failed to open " << argv[2] << endl;
+		return 1;
+	}
+
+	ks1 = kseq_init(fp1);
+
+	int buffer_size = 1<<20;//1MB
+	int buffer_pos = 0;
+	vector<char> buffer(buffer_size);
+
+	int count = 0;	
+
+	while(1)
+	{
+		int length = kseq_read(ks1);
+
+		if(length < 0) break;
+
+		char * seq1 = ks1->seq.s;
+		cout << ks1->name.s << " ";
+		cout << ks1->comment.s << endl;
+		KHFMinHash mh = KHFMinHash(seq1);			
 	
-	auto & sketch = mh.getSektch();	
+		auto & sketch = mh.getSektch();	
 
-	for(int i = 0; i < sketch.hashes.size(); i++)
-		cout << sketch.hashes[i] << endl;
+		//for(int i = 0; i < sketch.hashes.size(); i++)
+		//	cerr << sketch.hashes[i] << endl;	
+
+		const char* vec_data = reinterpret_cast<const char*>(sketch.hashes.data());
+		int vec_bytes_left = sketch.hashes.size() * sizeof(uint64_t);
+		int vec_pos = 0;
+		while (vec_bytes_left > 0)
+		{
+			size_t bytes_to_copy = std::min(buffer_size - buffer_pos, vec_bytes_left);
+			std::memcpy(buffer.data() + buffer_pos, vec_data + vec_pos, bytes_to_copy);
+			buffer_pos += bytes_to_copy;
+			vec_pos += bytes_to_copy;
+			vec_bytes_left -= bytes_to_copy;
+
+			if(buffer_pos == buffer_size)
+			{
+				ofile.write(buffer.data(), buffer_size);
+				buffer_pos = 0;
+			}
+		}
+
+		count++;
+	}
+	
+	if(buffer_pos > 0)
+		ofile.write(buffer.data(), buffer_pos);
+
+    gzclose(fp1);
+    kseq_destroy(ks1);
+
+	ofile.close();
+	cerr << "number of seqs: " << count << endl;
 
 	return 0;
 	
