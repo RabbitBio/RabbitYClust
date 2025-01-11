3b1a9e7d328e89ca36018b3129f50d6722b71797
yangyang <1115418865@qq.com>
2025-01-12 01:44:17 +0800

    Test the clustering function of cdhit on a single group

---
+++ kernels/cluster-test/cluster.cpp
@@ -0,0 +1,87 @@
+#include "cluster.h"
+
+Options options;
+//SequenceDB seq_db;
+
+void cluster::cdhit_cluster(std::vector<Sequence_new>& seq, std::vector<int>& parent)
+{
+
+//	Options options;
+	SequenceDB seq_db;
+//    options = Options();
+    seq_db = SequenceDB();
+
+    string db_out;
+
+    if (seq.size() == 0) {
+        std::cout<<"Empty input sequence"<<std::endl;
+        return;
+    }
+
+    options.SetOptionsInputVector();
+    options.Validate();
+
+    db_out = options.output;
+    InitNAA(MAX_UAA);
+    options.NAAN = NAAN_array[options.NAA];
+    seq_db.NAAN = NAAN_array[options.NAA];
+
+    seq_db.Readvector(seq, options);
+
+    cout << "total seq: " << seq_db.sequences.size() << endl;
+    seq_db.SortDivide(options);
+
+    seq_db.DoClustering(options);
+
+	seq_db.updateParent(parent);
+}
+
+int main()
+{
+	vector<Sequence_new> sequences;
+	string filename = "test.fa";
+	ifstream file(filename);
+	std::string line;
+    int currentId = -1; // 当前的 ID，初始值为无效值
+    std::string currentData; // 用于临时存储序列内容
+
+	while(getline(file, line)) {
+        stringstream ss(line);
+        int idCandidate;
+
+		if (ss >> idCandidate && ss.eof()) {
+				// 如果读取成功并且整行是一个整数
+				if (currentId != -1 && !currentData.empty()) {
+						// 保存之前的记录
+						Sequence_new seq;
+						seq.seq_id = currentId;
+						seq.data = new char[currentData.size() + 1];
+						strcpy(seq.data, currentData.c_str());
+						sequences.push_back(seq);
+				}
+				// 更新当前 ID，并清空当前序列内容
+				currentId = idCandidate;
+				currentData.clear();
+		} else {
+				// 当前行是序列的一部分，追加到 currentData 中
+				currentData += line;
+		}
+	}
+
+    if (currentId != -1 && !currentData.empty()) {
+        Sequence_new seq;
+        seq.seq_id = currentId;
+        seq.data = new char[currentData.size() + 1];
+        std::strcpy(seq.data, currentData.c_str());
+        sequences.push_back(seq);
+    }
+
+    file.close();
+
+	vector<int> parent(currentId + 1, 0);
+	cluster cls;
+	cls.cdhit_cluster(sequences, parent);
+	for(int i = 0 ; i <= currentId; i++){
+		cout << i << " " << parent[i] << endl;
+	}
+}
