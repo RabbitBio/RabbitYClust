443d93a88c69a09d54196541c62d4915ad2a369c
yangyang <1115418865@qq.com>
2025-04-11 15:05:59 +0800

    1.delete main.cpp inherit from cdhit-branch; 2. modify the reading in kernels/cluster.cpp to test cdhit.

--- kernels/cluster-test/cluster.cpp
+++ kernels/cluster-test/cluster.cpp
@@ -39,7 +39,7 @@ void cluster::cdhit_cluster(std::vector<Sequence_new>& seq, std::vector<int>& pa
 int main()
 {
 	vector<Sequence_new> sequences;
-	string filename = "test.fa";
+	string filename = "test-update.fa";
 	ifstream file(filename);
 	std::string line;
     int currentId = -1; // 当前的 ID，初始值为无效值
@@ -49,23 +49,42 @@ int main()
         stringstream ss(line);
         int idCandidate;
 
-		if (ss >> idCandidate && ss.eof()) {
-				// 如果读取成功并且整行是一个整数
-				if (currentId != -1 && !currentData.empty()) {
-						// 保存之前的记录
-						Sequence_new seq;
-						seq.seq_id = currentId;
-						seq.data = new char[currentData.size() + 1];
-						strcpy(seq.data, currentData.c_str());
-						sequences.push_back(seq);
-				}
+		if(line.empty()) continue;
+
+        if (line[0] == '>') {  // 如果以 '>' 开头
+            if (currentId != -1 && !currentData.empty()) {
+				// 保存之前的记录
+				Sequence_new seq;
+				seq.seq_id = currentId;
+				seq.data = new char[currentData.size() + 1];
+				strcpy(seq.data, currentData.c_str());
+				sequences.push_back(seq);
+            }
 				// 更新当前 ID，并清空当前序列内容
-				currentId = idCandidate;
-				currentData.clear();
-		} else {
-				// 当前行是序列的一部分，追加到 currentData 中
-				currentData += line;
-		}
+			currentId++;
+			currentData.clear();
+        } else {
+            currentData += line;  // 拼接序列行
+        }
+
+//        int idCandidate;
+//		if (ss >> idCandidate && ss.eof()) {
+//				// 如果读取成功并且整行是一个整数
+//				if (currentId != -1 && !currentData.empty()) {
+//						// 保存之前的记录
+//						Sequence_new seq;
+//						seq.seq_id = currentId;
+//						seq.data = new char[currentData.size() + 1];
+//						strcpy(seq.data, currentData.c_str());
+//						sequences.push_back(seq);
+//				}
+//				// 更新当前 ID，并清空当前序列内容
+//				currentId = idCandidate;
+//				currentData.clear();
+//		} else {
+//				// 当前行是序列的一部分，追加到 currentData 中
+//				currentData += line;
+//		}
 	}
 
     if (currentId != -1 && !currentData.empty()) {
@@ -77,11 +96,19 @@ int main()
     }
 
     file.close();
-
+	cout << sequences.size() << endl;
 	vector<int> parent(currentId + 1, 0);
 	cluster cls;
 	cls.cdhit_cluster(sequences, parent);
-	for(int i = 0 ; i <= currentId; i++){
-		cout << i << " " << parent[i] << endl;
+//	for(int i = 0 ; i <= currentId; i++){
+//		cout << i << " " << parent[i] << endl;
+//	}
+	vector<int> t_parent1(currentId + 1, 1);
+	vector<int> t_parent2(currentId + 1, 0);
+
+	cls.cdhit_cluster(sequences, t_parent1);
+	cls.cdhit_cluster(sequences, t_parent2);
+	for(int i = 0; i <= currentId; i++){
+		cout << parent[i] << " " << t_parent1[i] << t_parent2[i] << endl;
 	}
 }
